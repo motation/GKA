73d173a2a7d19825db54e676cc1e28c19f6b5ba4
CBaderschneider <claudius.baderschneider@haw-hamburg.de>
2015-04-29 19:02:23 +0200

    Dijkstra test files Not implementet

---
+++ src/algorthmTest/TestDijkstra.java
@@ -0,0 +1,14 @@
+package algorthmTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestDijkstra {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
