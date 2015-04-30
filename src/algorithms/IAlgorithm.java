c52d94238145f40541b962e1890e229dbfff89e7
motation <sven-ole.fedders@haw-hamburg.de>
2015-04-30 11:39:41 +0200

    interface modification

--- src/algorithms/IAlgorithm.java
+++ src/algorithms/IAlgorithm.java
@@ -1,6 +1,15 @@
 package algorithms;
 
+import org.jgrapht.GraphPath;
+
+import elements.IGraph;
+import elements.IVertex;
+
 
 public interface IAlgorithm {
-	
+	public void setStartVertx(IVertex startVertx);
+	public void setEndVertex(IVertex endVertex);
+	public void calculate();
+	public GraphPath getShortestPath();
+	public IGraph getGraph();
 }
--- src/main/MainApplication.java
+++ src/main/MainApplication.java
@@ -1,10 +1,16 @@
 package main;
 
+import elements.IGraph;
+import elements.IVertex;
 import gui.StartWindow;
 
+import java.util.List;
 import java.io.IOException;
 
 import javax.swing.JFrame;
+
+import org.jgraph.graph.Edge;
+import org.jgrapht.GraphPath;
 
 public class MainApplication {
 	public static void main(String[] args) throws IOException {
@@ -12,5 +18,24 @@ public class MainApplication {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(768,500);
 		frame.setVisible(true);
+		
+		
+//		
+//		GraphPath<IVertex, Edge> path=null;
+//		
+//		List<Edge> edges = path.getEdgeList();
+//		
+//		IGraph graph = null;
+//		
+//		
+//		for(Edge edge : edges){
+//			
+//			IVertex source = (IVertex)edge.getSource();
+//			IVertex target = (IVertex)edge.getTarget();
+//			
+//			graph.addVertex(source);
+//			graph.addVertex(target);
+//			graph.addEdge(source, target, edge);
+//		}
 	}
 }
