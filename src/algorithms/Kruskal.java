d7d8f3eb01bd7dad96362814371e6f3f4403f81f
CBaderschneider <claudius.baderschneider@haw.de>
2015-05-26 18:20:40 +0200

    Created Kruskal and prim algorythem Classes

---
+++ src/algorithms/Kruskal.java
@@ -0,0 +1,39 @@
+package algorithms;
+
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+import org.jgraph.graph.Edge;
+import org.jgrapht.GraphPath;
+import org.jgrapht.graph.GraphPathImpl;
+
+
+
+import elements.IGraph;
+import elements.IVertex;
+
+public class Kruskal {
+
+
+	private IGraph CompleteGraph;
+	private IGraph SCC;
+	
+	public boolean hasCircle(){
+		
+		
+		return false;
+	}
+	
+	public IGraph compute(){
+		
+		
+		return null;
+		
+	}
+	
+}
