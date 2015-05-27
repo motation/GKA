0f4a16eb63fd84a648be21a00da89d0b8f66b3d2
CBaderschneider <claudius.baderschneider@haw.de>
2015-05-27 13:14:48 +0200

    Added function hasCircle, added function reachable

--- src/algorithms/Kruskal.java
+++ src/algorithms/Kruskal.java
@@ -22,18 +22,51 @@ public class Kruskal {
 
 	private IGraph CompleteGraph;
 	private IGraph SCC;
+	private Set<Edge> EdgeSet = new HashSet<Edge>();
 	
-	public boolean hasCircle(){
-		
-		
-		return false;
+	
+	
+
+	
+	public Kruskal(IGraph completeGraph, Set<Edge> vertexSet) {
+		super();
+		CompleteGraph = completeGraph;
+		this.EdgeSet = completeGraph.getAllEdges();
 	}
-	
+
 	public IGraph compute(){
 		
 		
 		return null;
 		
+	}
+	
+	public boolean reachable(IVertex start, IVertex end,IGraph graph){
+		Set<Edge> tempEdgeSet = graph.getOutgoingEdges(start);
+		for(Edge edge: tempEdgeSet){
+			if( edge.getTarget() == end){
+				return true;
+			}
+			reachable((IVertex) edge.getTarget(),end,graph);
+		}
+		return false;
+	}
+	
+	public boolean hasCircle(IGraph graph){
+		
+		Set<IVertex> tempVertexSet = graph.getGraph().vertexSet();
+		//ein Graph enthält einen Kreis falls es ein Knotentupel (IVertex1, IVertex2) gibt für das gilt:
+		// IVertex 2 ist von IVertex 1 aus erreichbahr und IVertex 1 ist von IVertex 2 aus erreichbahr
+		for(IVertex vertex: tempVertexSet){	
+			Set<IVertex> tempVertexSet2 = tempVertexSet;
+			tempVertexSet2.remove(vertex);
+			for(IVertex vertex2: tempVertexSet2){
+				if((reachable(vertex,vertex2,graph) && reachable(vertex2,vertex,graph))){
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 	
 }
