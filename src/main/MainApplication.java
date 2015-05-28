15e724b8ff62a2c0eec520fbce5ef63b62b4b0d6
motation <sven-ole.fedders@haw-hamburg.de>
2015-05-28 03:02:58 +0200

    main window clean up

--- src/main/MainApplication.java
+++ src/main/MainApplication.java
@@ -12,24 +12,5 @@ public class MainApplication {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(768,500);
 		frame.setVisible(true);
-		
-		
-//		
-//		GraphPath<IVertex, Edge> path=null;
-//		
-//		List<Edge> edges = path.getEdgeList();
-//		
-//		IGraph graph = null;
-//		
-//		
-//		for(Edge edge : edges){
-//			
-//			IVertex source = (IVertex)edge.getSource();
-//			IVertex target = (IVertex)edge.getTarget();
-//			
-//			graph.addVertex(source);
-//			graph.addVertex(target);
-//			graph.addEdge(source, target, edge);
-//		}
 	}
 }
