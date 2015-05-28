cc3207edddcbbbb88c244cc617711a2a53907062
motation <sven-ole.fedders@haw-hamburg.de>
2015-05-28 03:03:40 +0200

    creator for prim algorithm

---
+++ src/graphs/creator/PrimGenerator.java
@@ -0,0 +1,52 @@
+package graphs.creator;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import elements.IGraph;
+import elements.IVertex;
+import elements.Vertex;
+import elements.WeightedEdge;
+import graphs.UndirectedWeightedGraph;
+
+public class PrimGenerator {
+	public static IGraph createRandomPrimGraph(int numVertex,int numEdges){
+		IGraph graph = UndirectedWeightedGraph.createNewGraph();
+		
+		// add vertexes
+		for(int i=0;i<numVertex;i++){
+			IVertex source = new Vertex(String	.valueOf(createLetter()));
+			IVertex target = new Vertex(String.valueOf(createLetter()));
+			graph.addVertex(source);
+			graph.addVertex(target);	
+		}
+		
+		//add Edges
+		List<IVertex> vertexList = new ArrayList<IVertex>(graph.getGraph().vertexSet());
+		int count = 0;
+		while(count < numEdges){
+			double weight = createRandomInt(1, 100);
+			IVertex source = vertexList.get((createRandomInt(0, vertexList.size()-1)));
+			IVertex target = vertexList.get((createRandomInt(0, vertexList.size()-1)));
+			WeightedEdge edge = new WeightedEdge(source, target, weight);
+			graph.addEdge(source, target, edge);
+			WeightedEdge edge2 = new WeightedEdge(target, source, weight);
+			graph.addEdge(target, source, edge2);
+			count++;
+		}
+		
+		
+		return graph;
+	}
+	
+	private static char createLetter(){
+		return (char)createRandomInt(65,90);
+	}
+	
+	private static int createRandomInt(int low, int high){
+		Random r = new Random();
+		int result = r.nextInt(high - low + 1) + low;
+		return result;
+	}
+}
