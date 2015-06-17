59c44ddfb0a35dbe0f2188c9beefec49fd851512
motation <sven-ole.fedders@haw-hamburg.de>
2015-06-17 12:56:44 +0200

    added tests for euler generator

--- src/graphs/creator/EulerGenerator.java
+++ src/graphs/creator/EulerGenerator.java
@@ -1,8 +1,47 @@
 package graphs.creator;
 
+import elements.IGraph;
+import elements.IVertex;
+import elements.Vertex;
+import elements.WeightedEdge;
+import graphs.UndirectedWeightedGraph;
+import org.jgraph.graph.Edge;
+import org.jgrapht.alg.DijkstraShortestPath;
+
 /**
  * Created by Ole on 17.06.2015.
  */
 public class EulerGenerator {
+    public static IGraph generateEulerCircle(){
+        IGraph graph = UndirectedWeightedGraph.createNewGraph();
+        //OF TODO generate EulerCircle
+        return graph;
+    }
 
+    public static IGraph generateWrongEulerCircle(){
+        IGraph graph = UndirectedWeightedGraph.createNewGraph();
+        //OF TODO generate WrongEulerCircle
+
+        return graph;
+    }
+
+    public static boolean isEulerCircle(IGraph graph){
+        boolean isEuler=true;
+        IVertex randomVertex = graph.getGraph().vertexSet().iterator().next();
+        for(IVertex vertex : graph.getGraph().vertexSet()){
+            if(graph.getGraph().edgesOf(vertex).size()%2!=0){
+                return false;
+            }
+            if(!vertex.equals(randomVertex)){
+                try {
+                    if(DijkstraShortestPath.findPathBetween(graph.getGraph(),vertex,randomVertex).size() == 0){
+                        return false;
+                    }
+                } catch (NullPointerException e) {
+                    return false;
+                }
+            }
+        }
+        return isEuler;
+    }
 }
--- test/graphs/creator/EulerGeneratorTest.java
+++ test/graphs/creator/EulerGeneratorTest.java
@@ -1,6 +1,11 @@
 package graphs.creator;
 
+import elements.IGraph;
+import io.FileGraphReader;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
@@ -9,9 +14,77 @@ import static org.junit.Assert.*;
  */
 public class EulerGeneratorTest {
 
+    private FileGraphReader reader;
+
+    private IGraph eulerCircleTrue1;
+    private IGraph eulerCircleTrue2;
+    private IGraph eulerCircleTrue3;
+
+    private IGraph eulerCircleFalse1;
+    private IGraph eulerCircleFalse2;
+    private IGraph eulerCircleFalse3;
+
+    private IGraph staticEulerCircleTrue;
+    private IGraph staticEulerCircleFalse;
+
+
     @Before
     public void setUp() throws Exception {
 
+        reader = new FileGraphReader();
+
+        this.eulerCircleTrue1 = EulerGenerator.generateEulerCircle();
+        this.eulerCircleTrue2 = EulerGenerator.generateEulerCircle();
+        this.eulerCircleTrue3 = EulerGenerator.generateEulerCircle();
+
+        this.eulerCircleFalse1 = EulerGenerator.generateEulerCircle();
+        this.eulerCircleFalse2 = EulerGenerator.generateWrongEulerCircle();
+        this.eulerCircleFalse3 = EulerGenerator.generateWrongEulerCircle();
+
+        String stringGraphPositive =    "#weighted\n" +
+                                        "A,B::1\n" +
+                                        "A,D::2\n" +
+                                        "B,C::3\n" +
+                                        "D,C::4\n" +
+                                        "D,G::5\n" +
+                                        "C,G::6\n" +
+                                        "D,E::7\n" +
+                                        "C,F::8\n" +
+                                        "F,E::9";
+
+        String stringGraphNegative =    "#weighted\n" +
+                                        "A,B::1\n" +
+                                        "A,C::3\n" +
+                                        "B,C::2\n" +
+                                        "D,E::4\n" +
+                                        "D,F::5\n" +
+                                        "E,F::6";
+
+        this.staticEulerCircleTrue = reader.loadGraphByString(stringGraphPositive);
+        this.staticEulerCircleFalse = reader.loadGraphByString(stringGraphNegative);
     }
-    
+
+    @Test @Ignore
+    public void positiveTest() {
+        Assert.assertTrue(EulerGenerator.isEulerCircle(eulerCircleTrue1));
+        Assert.assertTrue(EulerGenerator.isEulerCircle(eulerCircleTrue2));
+        Assert.assertTrue(EulerGenerator.isEulerCircle(eulerCircleTrue3));
+    }
+
+    @Test
+    public void negativeTest() {
+        Assert.assertFalse(EulerGenerator.isEulerCircle(eulerCircleFalse1));
+        Assert.assertFalse(EulerGenerator.isEulerCircle(eulerCircleFalse2));
+        Assert.assertFalse(EulerGenerator.isEulerCircle(eulerCircleFalse3));
+    }
+
+    @Test
+    public void staticPositiveTest() {
+        Assert.assertTrue(EulerGenerator.isEulerCircle(staticEulerCircleTrue));
+    }
+
+    @Test
+    public void staticNegativeTest() {
+        Assert.assertFalse(EulerGenerator.isEulerCircle(staticEulerCircleFalse));
+    }
 }
